9c60510efc9e3292406485fdcac6e3f31fdf642b
Murillo Melo <31209408+murillomelo@users.noreply.github.com>
2019-04-24 10:07:48 -0300

    Create shutdown.py

---
+++ shutdown.py
@@ -0,0 +1,13 @@
+import boto3
+
+
+def lambda_handler(event, context):
+    region = 'sa-east-1'
+
+    resource = boto3.resource('ec2')
+    client = boto3.client('ec2', region_name=region)
+
+    for instance in resource.instances.filter(Filters=[{'Name': 'tag:Shutdown', 'Values': ['True']}]):
+        resultado = instance.id
+        client.stop_instances(InstanceIds=[str(resultado)])
+        print('Stop Instance: ' + str(resultado))
